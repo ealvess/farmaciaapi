f06e116cc62fdff7c68f68b3e620bbf84c7981a0
ealvess <everton10alves@gmail.com>
2020-07-13 00:40:04 -0300

    Configurando profiles do spring

--- src/main/java/com/farmacia/farmaciaapi/FarmaciaapiApplication.java
+++ src/main/java/com/farmacia/farmaciaapi/FarmaciaapiApplication.java
@@ -2,8 +2,12 @@ package com.farmacia.farmaciaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.farmacia.farmaciaapi.config.property.FarmaciaApiProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FarmaciaApiProperty.class)
 public class FarmaciaapiApplication {
 
 	public static void main(String[] args) {
